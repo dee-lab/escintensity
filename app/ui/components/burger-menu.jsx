9d6ee46d50434e5fbd4e0f369b44ab1bcf563c03
Dixie <dixiexdigital@gmail.com>
2024-04-10 11:05:44 +0100

    Add burger menu

---
+++ app/ui/components/burger-menu.jsx
@@ -0,0 +1,9 @@
+import './burger-menu.css';
+
+const BurgerMenuIcon = () => (
+  <label className="burger-menu">
+    <input type="checkbox" />
+  </label>
+);	
+
+export default BurgerMenuIcon;
--- app/ui/nav.jsx
+++ app/ui/nav.jsx
@@ -1,3 +1,4 @@
+import BurgerMenuIcon from './components/burger-menu';
 
 const NAVITEMS = [
     {"label": "Home", "link": "./", "active": true},
@@ -14,15 +15,16 @@ const NAVITEMS = [
 const NavBar = () => {
     return (
         <nav className="main-nav bg-zinc-950 text-white sticky top-0 z-10 ">
-        <ul className="flex justify-end items-center list-none m-0 px-0">
-            {NAVITEMS.map((navItem, index) => {
-                return (
-                <li key={index} className="uppercase py-3.5 px-4 no-underline hover:bg-red-800 hover:cursor-pointer">
-                    <a href={navItem.link}>{navItem.label}</a>
-                </li>
-                )            
-            })}
-        </ul>
+            <BurgerMenuIcon />
+            <ul className="flex justify-end items-center list-none m-0 px-0">
+                {NAVITEMS.map((navItem, index) => {
+                    return (
+                    <li key={index} className="uppercase py-3.5 px-4 no-underline hover:bg-red-800 hover:cursor-pointer">
+                        <a href={navItem.link}>{navItem.label}</a>
+                    </li>
+                    )            
+                })}
+            </ul>
         </nav>
     )
 }
