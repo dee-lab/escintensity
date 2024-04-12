a913535556a2c3c0316e2efb47482dde16a658e3
Dixie <dixiexdigital@gmail.com>
2024-04-12 09:50:53 +0100

    Add top border to footer

--- app/ui/footer.jsx
+++ app/ui/footer.jsx
@@ -3,10 +3,10 @@ import {Input} from './components/input'
 const Footer = () => {
   return (
    <div className='flex flex-col-reverse md:flex-row justify-between gap-14 md:items-end bg-zinc-950 p-9 px-10 lg:px-40 text-white'>
-     <footer>
+     <footer className='p-4 mt-4 border-t border-white/10'>
       Copyright © 2024 ESC Intensity
     </footer>
-    <Input />
+    <Input /> 
    </div>
   )
 }
