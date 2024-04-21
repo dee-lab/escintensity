73d474a51de11604916bac2adcd26f89ad67d46f
Dixie <dixiexdigital@gmail.com>
2024-04-21 21:21:37 +0100

    Add missing button props to contact section

--- app/ui/contact.jsx
+++ app/ui/contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section className="bg-zinc-300 py-16">
       <div className="flex flex-col text-zinc-900 text-center items-center gap-8 ms-auto me-auto max-w-[80%]">
         <h3 className="text-4xl my-5">Do you dream of a healthier, stronger you but lack the guidance to get there?</h3>
-        <Button>
+        <Button type="primary" size="md">
           <span>Get in touch</span>
         </Button>        
       </div>
