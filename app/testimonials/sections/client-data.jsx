f3949cc5457454d3087be2402f023317f34e88a9
Dixie <dixiexdigital@gmail.com>
2024-04-24 21:24:41 +0100

    Style the testimonial page

--- app/testimonials/sections/client-data.jsx
+++ app/testimonials/sections/client-data.jsx
@@ -2,7 +2,7 @@ const CLIENTS = [
   {
     name: "Daniel O'Connell" ,
     image: "/client-results/client-r-daniel-o-connell.png",
-    testimonial: "I was recommended to syxx by a friend . We discussed what my goals were and he created a meal & workout plan that I could achieve within my busy family & work life. Weekly check ins were simple to do and feedback was prompt. Any issues or advice I needed he was only a message away. I was very happy with my transformation and would definitely recommend him.",
+    testimonial: "I was recommended to Syxx by a friend . We discussed what my goals were and he created a meal & workout plan that I could achieve within my busy family & work life. Weekly check ins were simple to do and feedback was prompt. Any issues or advice I needed he was only a message away. I was very happy with my transformation and would definitely recommend him.",
   },
   {
     name: "Sarah King" ,
@@ -12,17 +12,17 @@ const CLIENTS = [
   {
     name: "Gem Williams" ,
     image: "/client-results/client-r-gem-williams.png",
-    testimonial: "Working with syxx was a great decision. Each week he went through my plans with a fine tooth comb and nothing was missed, my goals were met and he was available anytime via voice note, text or call. I have a scoliosis and was nervous about lifting weights but syxx created a plan that was tailored for me and improved my muscle balance. He made my whole experience fun with his banter and energy. I'd highly recommend him.",
+    testimonial: "Working with Syxx was a great decision. Each week he went through my plans with a fine tooth comb and nothing was missed, my goals were met and he was available anytime via voice note, text or call. I have a scoliosis and was nervous about lifting weights but Syxx created a plan that was tailored for me and improved my muscle balance. He made my whole experience fun with his banter and energy. I'd highly recommend him.",
   },
   {
     name: "Ludwig A." ,
     image: "/client-results/client-r-ludwig-a.png",
-    testimonial: "Syxx has been pivotal to the way that I have not only changed and made my body healthier, but also the way that I now look at fitness and nutrition, Syxx is somebody who is very persistent, and insures he gets the best out of his clients. I have been using Syxx on and off whenever I need to get in shape for the last three years and I will continue to use him for as long as possible as he's been the only coach/PT that has delivered results in my corner.",
+    testimonial: "Syxx has been pivotal to the way that I have not only changed and made my body healthier, but also the way that I now look at fitness and nutrition. Syxx is somebody who is very persistent, and insures he gets the best out of his clients. I have been using Syxx on and off whenever I need to get in shape for the last three years and I will continue to use him for as long as possible as he's been the only coach/PT that has delivered results in my corner.",
   },
   {
     name: "Nora Csaszar" ,
     image: "/client-results/client-r-nora-csaszar.png",
-    testimonial: "Syxx has been pivotal to the way that I have not only changed and made my body healthier, but also the way that I now look at fitness and nutrition, Syxx is somebody who is very persistent, and insures he gets the best out of his clients. I have been using Syxx on and off whenever I need to get in shape for the last three years and I will continue to use him for as long as possible as he's been the only coach/PT that has delivered results in my corner.",
+    testimonial: "I started working with Syxx about a year ago.  He helped me transform my physique and mindset as well. He created a meal and workout plan that works with my busy work life. Weekly check-ins and feedbacks are always on time. He's super encouraging and always there to help.  I'm extremely happy with my transformation, I couldn't have done it without his support.  I highly recommend him.",
   },
 ]
 
--- app/testimonials/sections/client-testimonials.jsx
+++ app/testimonials/sections/client-testimonials.jsx
@@ -5,21 +5,24 @@ import CLIENTS from "./client-data";
 
 const ClientTestimonials = () => {
   return (
-    <section className="bg-white py-16">
+    <>
       {CLIENTS.map((client, index) => {
         return ( 
-          <section key={index} className="flex flex-wrap *:grow *:shrink *:basis-1/2 gap-4 *:text-center *:p-2 *:max-w-96 *:min-w-96">
+          <section key={index} className="bg-white flex flex-col md:flex-row md:odd:flex-row-reverse justify-center items-center gap-8 py-8 odd:bg-zinc-100 text-justify *:grow *:shrink *:basis-1/2 *:max-w-96 *:min-w-96">
             <Image
               src={client.image}
               alt={`Client testimonial from ${client.name}`}
               height={300}
               width={300}
             />
-            <p>{client.testimonial}</p>
+            <div>
+              <q className="italic">{client.testimonial}</q>
+              <p className="pt-2 font-semibold">{client.name}</p>
+            </div>
           </section>
         )
       })}
-    </section>
+    </>
   )
 }
 
