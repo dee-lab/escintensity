523f95e2d98f3dcf50ce42f8b91cae9e81717143
Dixie <dixiexdigital@gmail.com>
2024-04-29 23:12:10 +0100

    Add waitlist placeholder landing page

--- app/page.js
+++ app/page.js
@@ -5,11 +5,11 @@ export default function LandingPage() {
   return (
     <> 
       <main style={{display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", alignItems: "center", color: "white", textTransform: "uppercase", letterSpacing: ".2rem", textAlign: "center"}}>
-        <Image 
+      <Image 
           src="/logo-white.png"
+          width={400}
+          height={400}
           alt="ESC Intensity logo"
-          fill
-          objectFit="contain"
         />
         <h1>🚧 Website under construction 🚧</h1>
       </main>
---
+++ app/waitlist/page.jsx
@@ -0,0 +1,23 @@
+"use client"
+import Image from "next/image";
+
+export default function WaitList() {
+  return (
+    <> 
+      <main style={{display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", alignItems: "center", color: "white", textTransform: "uppercase", letterSpacing: ".2rem", textAlign: "center"}}>
+        
+        <h1>ESC Community Wait List</h1>
+        <h3>coming soon</h3>
+        <Image 
+          src="/logo-white.png"
+          width={300}
+          height={300}
+          alt="ESC Intensity logo"
+        />
+      </main>
+    </>
+  );
+}
+
+
+
