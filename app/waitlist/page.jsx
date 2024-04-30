4f7e4bb18cb20c3c0aa39f52722aee3fda624249
Dixie <dixiexdigital@gmail.com>
2024-04-30 22:47:18 +0100

    Embed mailerlite waitlist form

--- app/waitlist/page.jsx
+++ app/waitlist/page.jsx
@@ -1,23 +1,18 @@
 "use client"
-import Image from "next/image";
+import Script from 'next/script'
 
 export default function WaitList() {
   return (
     <> 
-      <main style={{display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", alignItems: "center", color: "white", textTransform: "uppercase", letterSpacing: ".2rem", textAlign: "center", paddingTop: "48px"}}>
-        
-        <h1>ESC Community Wait List</h1>
-        <h2>coming soon</h2>
-        <Image 
-          src="/logo-white.png"
-          width={300}
-          height={300}
-          alt="ESC Intensity logo"
-        />
-      </main>
+      <Script id="mailerlite" 
+        strategy="beforeInteractive" 
+        dangerouslySetInnerHTML={{
+      __html: `(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+          .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+          n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+          (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+          ml('account', '927700');`}} />
+      <div class="ml-embedded" data-form="hdKVkQ"></div>
     </>
   );
 }
-
-
-
