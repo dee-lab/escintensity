12defade5241f0f20c2bf1c161013231f35688d1
Dixie <dixiexdigital@gmail.com>
2024-06-04 22:52:57 +0100

    Add instagram link

--- app/toggle/ui/components/socials.jsx
+++ app/toggle/ui/components/socials.jsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Socials = () => {
   return (
     <section className="flex flex-nowrap justify-center py-8 md:py-0 gap-4">
-      <Image
+      {/* <Image
         src="/Facebook_Logo.png"
         width={29}
         height={29}
-        alt="Facebook Logo" />
+        alt="Facebook Logo" /> */}
+      <Link href="https://www.instagram.com/escintensity/">
         <Image
-        src="/Instagram_Logo.png"
-        width={29}
-        height={29}
-        alt="Instagram Logo" />
+          src="/Instagram_Logo.png"
+          width={29}
+          height={29}
+          alt="Instagram Logo" />
+      </Link>
     </section>
   )
 };
