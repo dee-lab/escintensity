573da17e3270aa67f67fa2139c551e5e24407484
Dixie <dixiexdigital@gmail.com>
2025-08-29 12:32:48 +0100

    Embed contact form to contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,18 @@
-'use client'
+"use client";
 
-import Header from "./sections/header";
+import Script from "next/script";
 
 export default function Contact() {
-  return (
-    <main>
-      <Header />
-    </main>
-  )
+	return (
+		<main>
+			<div formsappId='68b1749e131e1632cebcfd14'></div>
+			<Script
+				src='https://forms.app/cdn/embed.js'
+				type='text/javascript'
+				async
+				defer
+				onload="new formsapp('68b1749e131e1632cebcfd14', 'standard', {'width':'100vw','height':'100vh','opacity':0}, 'https://c96uct7k.forms.app');"
+			></Script>
+		</main>
+	);
 }
