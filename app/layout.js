b579c0212d1a1d776d951031b58cf6d23a1f6489
Dixie <dixiexdigital@gmail.com>
2025-08-29 21:41:34 +0100

    Add cookie script - test

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Footer from "./ui/footer";
 import { Orbitron, Kanit } from "next/font/google";
 import "./ui/globals.css";
 import "./ui/globalIcons.css";
+import Script from "next/script";
 
 // const orbitron = Orbitron({
 //   // weight: '400',
@@ -22,6 +23,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
+			<head>
+				<Script
+					id='Cookiebot'
+					src='https://consent.cookiebot.com/uc.js'
+					data-cbid='010f9f87-8729-4866-b0d1-5380ba7052c7'
+					data-blockingmode='auto'
+					type='text/javascript'
+				></Script>
+			</head>
 			<body className={kanit.className}>
 				<NavBar />
 				{children}
