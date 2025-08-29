24b0d5a43c9fc1ca5c47fe0e18a3397769ad8916
Dixie <dixiexdigital@gmail.com>
2025-08-29 13:14:48 +0100

    Remove toggle from button link

--- app/sections/header.jsx
+++ app/sections/header.jsx
@@ -1,18 +1,23 @@
-import { useRouter } from 'next/navigation'
-import { Button } from '../ui/components/button';
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/components/button";
 
 const Header = () => {
-  const router = useRouter()
-  return (
-    <header className="relative h-[480px] bg-right-top bg-[url('/fitness-500x510.jpg')] md:bg-[url('/fitness1080x450.png')] bg-cover bg-no-repeat grayscale-[90%]">          
-      <section className="absolute text-white top-[20%] left-[10%] max-w-[700px]">
-        <h1 className="text-7xl md:text-8xl">Every Session Counts</h1>
-        <Button variant ="primary" size="md" className="mt-12 mb-8" onClick={() => router.push('/toggle/what-to-expect')}>
-          <span>Start your journey</span>
-        </Button> 
-      </section>
-    </header>
-  )
-}
+	const router = useRouter();
+	return (
+		<header className="relative h-[480px] bg-right-top bg-[url('/fitness-500x510.jpg')] md:bg-[url('/fitness1080x450.png')] bg-cover bg-no-repeat grayscale-[90%]">
+			<section className='absolute text-white top-[20%] left-[10%] max-w-[700px]'>
+				<h1 className='text-7xl md:text-8xl'>Every Session Counts</h1>
+				<Button
+					variant='primary'
+					size='md'
+					className='mt-12 mb-8'
+					onClick={() => router.push("/what-to-expect")}
+				>
+					<span>Start your journey</span>
+				</Button>
+			</section>
+		</header>
+	);
+};
 
 export default Header;
