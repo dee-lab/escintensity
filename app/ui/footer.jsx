6f3643d8af5bd87bb7ee8785d048b8139ea4ff05
Dixie <dixiexdigital@gmail.com>
2025-09-23 20:14:11 +0100

    Remove the unclickable links from footer

--- app/ui/footer.jsx
+++ app/ui/footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
 		<footer className=' bg-zinc-950 text-white flex flex-col px-4'>
 			<Socials />
 			<div className='text-center md:text-left py-11 px-9'>
-				<ul className='flex flex-row gap-8 justify-center'>
+				<ul className='flex flex-row gap-8 justify-center lg:justify-end'>
 					<li>Copyright © 2025 ESC Coaching</li>
-					<li>Terms and Conditions</li>
+					{/* <li>Terms and Conditions</li>
 					<li>Privacy Policy</li>
-					<li>Cookie Policy</li>
+					<li>Cookie Policy</li> */}
 				</ul>
 			</div>
 		</footer>
