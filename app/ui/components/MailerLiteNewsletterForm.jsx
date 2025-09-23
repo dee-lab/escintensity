66495a27c2b05e3dbd361a4b51f2e7fcba92813f
Dixie <dixiexdigital@gmail.com>
2025-09-23 11:59:26 +0100

    Update newsletter sign up form

--- app/ui/components/MailerLiteNewsletterForm.jsx
+++ app/ui/components/MailerLiteNewsletterForm.jsx
@@ -1,34 +1,134 @@
-"use client";
+import { useState, useEffect } from "react";
+import "./NewsletterForm.css";
 
-import React, { useEffect } from "react";
+function NewsletterForm() {
+	const [submitted, setSubmitted] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-const MailerLiteNewsletterFormWrapper = () => {
 	useEffect(() => {
-		// This is the code from the original script tag
-		(function (w, d, e, u, f, l, n) {
-			w[f] =
-				w[f] ||
-				function () {
-					(w[f].q = w[f].q || []).push(arguments);
-				};
-			l = d.createElement(e);
-			l.async = 1;
-			l.src = u;
-			n = d.getElementsByTagName(e)[0];
-			n.parentNode.insertBefore(l, n);
-		})(
-			window,
-			document,
-			"script",
-			"https://assets.mailerlite.com/js/universal.js",
-			"ml"
+		// Load MailerLite webforms script
+		const script = document.createElement("script");
+		script.src =
+			"https://groot.mailerlite.com/js/w/webforms.min.js?v176e10baa5e7ed80d35ae235be3d5024";
+		script.async = true;
+		document.body.appendChild(script);
+
+		fetch(
+			"https://assets.mailerlite.com/jsonp/927700/forms/164233595473364218/takel"
 		);
 
-		// This is the call to the MailerLite function
-		window.ml("account", "927700");
-	}, []); // The empty dependency array ensures this effect runs only once when the component mounts
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
 
-	return <div class='ml-embedded' data-form='YXszcy'></div>;
-};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
 
-export default MailerLiteNewsletterFormWrapper;
+		const form = e.target;
+		const formData = new FormData(form);
+
+		fetch(form.action, {
+			method: form.method,
+			body: formData,
+			mode: "no-cors",
+		})
+			.then(() => {
+				setLoading(false);
+				setSubmitted(true);
+			})
+			.catch(() => {
+				setLoading(false);
+				setSubmitted(true);
+			});
+	};
+
+	return (
+		<div
+			id='mlb2-30297441'
+			className='ml-form-embedContainer ml-subscribe-form ml-subscribe-form-30297441 wrapper'
+		>
+			<div className='ml-form-align-center'>
+				<div className='ml-form-embedWrapper embedForm'>
+					<div
+						className={`ml-form-embedBody ml-form-embedBodyDefault row-form ${
+							submitted ? "fade-out" : "fade-in"
+						}`}
+						style={{ display: submitted ? "none" : "block" }}
+					>
+						<div className='ml-form-embedContent'>
+							<h4 className='text-4xl'>Stay in the know</h4>
+							<p>
+								Be the first one to receive new releases, special offers, and
+								more
+								{/* Subscribe to get exclusive access to expert Q&As, inspiring
+								community stories, special offers, and expert nutrition
+								tips—delivered straight to your inbox.  */}
+							</p>
+						</div>
+
+						<form
+							className='ml-block-form'
+							action='https://assets.mailerlite.com/jsonp/927700/forms/164233595473364218/subscribe'
+							method='post'
+							target='_blank'
+							onSubmit={handleSubmit}
+						>
+							<div className='ml-form-formContent'>
+								<div className='ml-form-fieldRow ml-last-item'>
+									<div className='ml-field-group ml-field-email ml-validate-email ml-validate-required'>
+										<input
+											aria-label='email'
+											aria-required='true'
+											type='email'
+											className='form-control'
+											name='fields[email]'
+											placeholder='Enter your email'
+											autoComplete='email'
+											required
+										/>
+									</div>
+								</div>
+							</div>
+
+							<input type='hidden' name='ml-submit' value='1' />
+							<input type='hidden' name='anticsrf' value='true' />
+
+							<div className='ml-form-embedSubmit'>
+								<button type='submit' className='primary' disabled={loading}>
+									{loading ? "Submitting..." : "Sign up"}
+								</button>
+
+								{loading && (
+									<button type='button' className='loading' disabled>
+										<div className='ml-spinner'></div>
+										<span className='sr-only'>Loading...</span>
+									</button>
+								)}
+							</div>
+						</form>
+					</div>
+
+					<div
+						className={`ml-form-successBody row-success ${
+							submitted ? "fade-in" : "fade-out"
+						}`}
+						style={{ display: submitted ? "block" : "none" }}
+					>
+						<div className='ml-form-successContent'>
+							<h4 className='text-4xl'>Almost there!</h4>
+							<p>
+								You've successfully signed up for our newsletter.
+								<br /> Just one more step to confirm your email and you'll be on
+								your way to receiving all our best content.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default NewsletterForm;
