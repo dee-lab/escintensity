6ef1510041eb008fa9a627cb3527382cf1d2ba7c
Dixie <dixiexdigital@gmail.com>
2025-09-23 22:42:59 +0100

    Update client result images

--- app/sections/client-results.jsx
+++ app/sections/client-results.jsx
@@ -1,41 +1,42 @@
-'use client'
+"use client";
 
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 import ImageCarousel from "../ui/components/carousel";
 
 const data = [
-  {cover: "/slider/image-1.png", title: "image 1"},
-  {cover: "/slider/image-2.png", title: "image 2"},
-  {cover: "/slider/image-3.jpg", title: "image 3"}, 
-  {cover: "/slider/image-4.png", title: "image 4"}, 
-  {cover: "/slider/image-5.jpg", title: "image 5"}, 
+	{ cover: "/slider/image-1.png", title: "image 1" },
+	{ cover: "/slider/image-2.png", title: "image 2" },
+	{ cover: "/slider/image-3.png", title: "image 3" },
+	{ cover: "/slider/image-4.png", title: "image 4" },
+	{ cover: "/slider/image-5.png", title: "image 5" },
+	{ cover: "/slider/image-6.png", title: "image 6" },
 ];
 
 const Slide = React.memo(function slide(props) {
-  const { data, dataIndex } = props;
-  const { cover } = data[dataIndex];
+	const { data, dataIndex } = props;
+	const { cover } = data[dataIndex];
 
-  return (
-    <div className='bg-neutral-50 rounded-xl'>
-      <Image
-        src={cover}
-        draggable={false}
-        alt="Transformational before and after picture" 
-        height={300}
-        width={300} 
-        className='m-2 inline-block'
-      />
-    </div>
-  );
+	return (
+		<div className='bg-neutral-50 rounded-xl'>
+			<Image
+				src={cover}
+				draggable={false}
+				alt='Transformational before and after picture'
+				height={300}
+				width={300}
+				className='m-2 inline-block'
+			/>
+		</div>
+	);
 });
 
 const ClientResultsSection = () => {
-  return (
-   <section className="bg-zinc-900 py-20">
-     <ImageCarousel slide={Slide} data={data}/>
-   </section>
-  )
- }
+	return (
+		<section className='bg-zinc-900 py-20'>
+			<ImageCarousel slide={Slide} data={data} />
+		</section>
+	);
+};
 
 export default ClientResultsSection;
